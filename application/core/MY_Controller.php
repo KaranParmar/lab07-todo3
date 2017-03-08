238041d375311881773a1bda35989175a26a8114
Karan Parmar <karandipparmar@hotmail.com>
2017-03-07 17:07:24 -0800

    Completed Job 9 - Incorporating Caboose

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -12,31 +12,46 @@
 class Application extends CI_Controller
 {
 
-	/**
-	 * Constructor.
-	 * Establish view parameters & load common helpers
-	 */
+    /**
+     * Constructor.
+     * Establish view parameters & load common helpers
+     */
+    function __construct()
+    {
+        parent::__construct();
 
-	function __construct()
-	{
-		parent::__construct();
+        //  Set basic view parameters
+        $this->data = array();
+        $this->data['pagetitle'] = 'TODO List Manager';
+        $this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '';
+         $this->data['alerts'] = '';
+        $this->error_free = TRUE;
+    }
 
-		//  Set basic view parameters
-		$this->data = array ();
-		$this->data['pagetitle'] = 'TODO List Manager';
-		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
-	}
-
-	/**
-	 * Render this page
-	 */
-	function render($template = 'template')
-	{
-        $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+    /**
+     * Render this page
+     */
+    function render($template = 'template')
+    {
+        $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'), true);
         // use layout content if provided
         if (!isset($this->data['content']))
             $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-		$this->parser->parse($template, $this->data);
-	}
+
+        // integrate any needed CSS framework & components
+        $this->data['caboose_styles'] = $this->caboose->styles();
+        $this->data['caboose_scripts'] = $this->caboose->scripts();
+        $this->data['caboose_trailings'] = $this->caboose->trailings();
+
+        $this->parser->parse($template, $this->data);
+    }
+
+    // Add an alert to the rendered page
+    function alert($message = '', $context = 'success')
+    {
+        $parms = ['message' => $message, 'context' => $context];
+        $this->data['alerts'] .= $this->parser->parse('_alert', $parms, true);
+        $this->error_free = FALSE;
+    }
 
 }
